2d8f03f8b69078d585b27f9fc3a6a34270f42db4
Mukhammadjon2002 <149391735+Mukhammadjon2002@users.noreply.github.com>
2023-11-20 09:40:26 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,148 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'HW3',
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => HomeScreen(),
+        '/screen1': (context) => Screen1(),
+        '/screen2': (context) => Screen2(),
+        '/screen3': (context) => Screen3(),
+        '/screen4': (context) => Screen4(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Home Screen'),
+        backgroundColor: const Color.fromARGB(255, 58, 183, 100),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 63, 181, 134),
+              Color.fromARGB(255, 58, 181, 183)
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome to the Home Screen!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/screen1');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.amber,
+                ),
+                child: Text(
+                  'Go to Screen 1',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Screen1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Screen 1'),
+        backgroundColor: Colors.blue,
       ),
       body: Center(
         child: Text(
-          'Hello, World!',
+          'This is Screen 1',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+class Screen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Screen 2'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text(
+          'This is Screen 2',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+class Screen3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Screen 3'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Text(
+          'This is Screen 3',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+class Screen4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Screen 4'),
+        backgroundColor: Colors.orange,
+      ),
+      body: Center(
+        child: Text(
+          'This is Screen 4',
+          style: TextStyle(fontSize: 24),
         ),
       ),
     );
